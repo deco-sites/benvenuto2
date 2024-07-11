23375367a0f062ec58b54c8bac86b3ef89cc78ed
Vitor Salek <vitorsalek@hotmail.com>
2024-07-11 01:25:57 -0300

    Update tables.ts

--- routes/sse/tables.ts
+++ routes/sse/tables.ts
@@ -1,9 +1,11 @@
 import { Handlers } from "$fresh/server.ts";
 import { ServerSentEventStream } from "std/http/server_sent_event_stream.ts";
 
+const DATABASE_ID = "2f0c2673-1eb4-407c-a605-b6914df02ae1"
+
 export const handler: Handlers = {
   async GET(_req) {
-    const kv = await Deno.openKv();
+    const kv = await Deno.openKv(`https://api.deno.com/databases/${DATABASE_ID}/connect`);
 
     return new Response(
       new ReadableStream({
