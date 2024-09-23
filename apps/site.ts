500395a760b630670dfaa22e6766ba95e8fdb26e
Vitor Salek <vitorsalek@hotmail.com>
2024-09-23 09:07:58 -0300

    Updated dependencies

--- apps/site.ts
+++ apps/site.ts
@@ -1,23 +1,18 @@
-import { App, AppContext as AC } from "deco/mod.ts";
 import website, { Props } from "apps/website/mod.ts";
-
 import manifest, { Manifest } from "../manifest.gen.ts";
-
+import { type App, type AppContext as AC } from "@deco/deco";
 type WebsiteApp = ReturnType<typeof website>;
-export default function Site(
-  state: Props,
-): App<Manifest, Props, [
-  WebsiteApp,
+export default function Site(state: Props): App<Manifest, Props, [
+    WebsiteApp
 ]> {
-  return {
-    state,
-    manifest,
-    dependencies: [
-      website(state),
-    ],
-  };
+    return {
+        state,
+        manifest,
+        dependencies: [
+            website(state),
+        ],
+    };
 }
-
 export type SiteApp = ReturnType<typeof Site>;
 export type AppContext = AC<SiteApp>;
 export { onBeforeResolveProps } from "apps/website/mod.ts";
---
+++ loaders/loaderGetMapFromRedis.ts
@@ -0,0 +1,22 @@
+import { TableMap } from "../static/MockedTableObject.tsx";
+import { Redis } from "@upstash/redis";
+
+export interface Props {
+  id: string;
+}
+
+export default async function loader(
+  { id }: Props,
+  _req: Request,
+): Promise<TableMap> {
+  const redis = new Redis({
+    url: Deno.env.get("UPSTASH_REDIS_REST_URL")!,
+    token: Deno.env.get("UPSTASH_REDIS_REST_TOKEN")!,
+  });
+
+  const key = `maps_couve_teste_${id}`;
+
+  const entry = await redis.get(key);
+
+  return entry as TableMap;
+}
