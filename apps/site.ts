71f4f17abbfa2e6de38d5dc4de957775b29e8d23
Marcos Candeia <marrcooos@gmail.com>
2024-11-13 17:50:07 -0300

    add redis props
    Signed-off-by: Marcos Candeia <marrcooos@gmail.com>

--- apps/site.ts
+++ apps/site.ts
@@ -1,9 +1,17 @@
-import website, { Props } from "apps/website/mod.ts";
+import { type AppContext as AC, type App } from "@deco/deco";
+import { Secret } from "apps/website/loaders/secret.ts";
+import website, { Props as WebsiteProps } from "apps/website/mod.ts";
 import manifest, { Manifest } from "../manifest.gen.ts";
-import { type App, type AppContext as AC } from "@deco/deco";
 type WebsiteApp = ReturnType<typeof website>;
+
+export interface Props extends WebsiteProps {
+    upstashRedis: {
+        url: string;
+        token: Secret;
+    };
+}
 export default function Site(state: Props): App<Manifest, Props, [
-    WebsiteApp
+    WebsiteApp,
 ]> {
     return {
         state,
